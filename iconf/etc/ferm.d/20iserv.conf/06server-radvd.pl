122f4cb35f18abcc0eeff9fbfb102ca24f335549
Felix Jacobi <Felix@jacobi-hamburg.net>
2018-12-16 04:11:36 +0100

    ferm: Fixed accidential dropping of foreign route advertisements

---
+++ iconf/etc/ferm.d/20iserv.conf/06server-radvd.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -CSDAL
+
+use warnings;
+use strict;
+
+print "domain (ip6) {\n";
+print "  table filter {\n";
+print "    chain input_first {\n";
+print "      # Prevent reading of our own route advertisements by dump daemons like rdnssd\n";
+for (qx(netquery6 -p -f "ip\tnic"))
+{
+  my ($ip, $nic) = split /\t/;
+  print "     proto ipv6-icmp icmpv6-type (133 134) saddr \@ipfilter($ip) ".
+      "interface $nic jump DROP;\n";
+}
+print "    }\n";
+print "  }\n";
+print "}\n";
+print "\n";
+
