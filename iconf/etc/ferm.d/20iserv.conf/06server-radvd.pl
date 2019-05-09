9b8e095bd40969c3d02008828fd9ba5a0355cc28
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-05-09 06:17:05 +0200

    Fixed typo in comment

--- iconf/etc/ferm.d/20iserv.conf/06server-radvd.pl
+++ iconf/etc/ferm.d/20iserv.conf/06server-radvd.pl
@@ -6,7 +6,7 @@ use strict;
 print "domain (ip6) {\n";
 print "  table raw {\n";
 print "    chain prerouting_linklocal {\n";
-print "      # Prevent reading of our own route advertisements by dump daemons like rdnssd\n";
+print "      # Prevent reading of our own route advertisements by dumb daemons like rdnssd\n";
 for (split /\n/, qx(netquery6 -p -f "ip\tnic"))
 {
   my ($ip, $nic) = split /\t/;
